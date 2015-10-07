b07871981d71a1d48b5b8956d92e0e926f7aeb64
debkalpa <debkalpa@gmail.com>
2015-10-07 12:27:04 -0400

    FlexiLab edit ATtiny84

--- SPI.cpp
+++ SPI.cpp
@@ -1,11 +1,7 @@
 /*
- * Copyright (c) 2010 by Cristian Maglie <c.maglie@bug.st>
- * SPI Master library for arduino.
- *
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of either the GNU General Public License version 2
- * or the GNU Lesser General Public License version 2.1, both as
- * published by the Free Software Foundation.
+ * Cross Platform SPI for ATtiny and ATmega.
+ * FlexiLab fork - October 7th, 2015
+ * original available at: https://github.com/jscrane/SPI
  */
 
 #include "pins_arduino.h"
